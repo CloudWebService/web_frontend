02d26417e899ef733d293661f9b00de53e893d85
nagyeong kim <skrud0318@naver.com>
2023-12-02 15:17:15 +0900

    feat:bottom bar 적용

--- seoultour/src/App.jsx
+++ seoultour/src/App.jsx
@@ -1,7 +1,7 @@
 import Router from "./pages";
 import Card from "./pages/RestaurantDetailPage";
 import AppBar from "./components/AppBar";
-
+import "./Styles/App.css";
 function App() {
   return <div className="App">main</div>;
 }
--- seoultour/src/index.js
+++ seoultour/src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import Router from "./Router";
 import AppBar from "./components/AppBar";
+import BottomBar from "./components/BottomBar";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <AppBar />
     <Router />
+    <BottomBar />
   </React.StrictMode>
 );
